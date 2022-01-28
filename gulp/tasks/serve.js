852ab4790342ce5d6b751700dbb032b5aa17caa1
twirlfog <twirlfog@gmail.com>
2022-01-28 09:25:59 +0300

    build: returned accidentally deleted lines with styles

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,15 +1,16 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 
-const imageMinify = require('./imageMinify')
-const svgSprite = require('./svgSprite')
-const pug2html = require('./pug2html')
-const copyDependencies = require('./copyDependencies')
+const imageMinify = require('./imageMinify');
+const svgSprite = require('./svgSprite');
+const styles = require('./styles');
+const pug2html = require('./pug2html');
+const copyDependencies = require('./copyDependencies');
 
-const server = require('browser-sync').create()
+const server = require('browser-sync').create();
 
 function readyReload(cb) {
-  server.reload()
-  cb()
+  server.reload();
+  cb();
 }
 
 module.exports = function serve(cb) {
@@ -18,13 +19,21 @@ module.exports = function serve(cb) {
     notify: false,
     open: true,
     cors: true
-  })
+  });
 
-  gulp.watch('src/img/*.{gif,png,jpg,svg,webp}', gulp.series(imageMinify, readyReload))
-  gulp.watch('src/img/sprite/*.svg', gulp.series(svgSprite, readyReload))
-  gulp.watch('src/styles/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)))
-  gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload))
-  gulp.watch('package.json', gulp.series(copyDependencies, readyReload))
+  gulp.watch(
+    'src/img/*.{gif,png,jpg,svg,webp}',
+    gulp.series(imageMinify, readyReload)
+  );
+  gulp.watch('src/img/sprite/*.svg', gulp.series(svgSprite, readyReload));
+  gulp.watch(
+    'src/styles/**/*.scss',
+    gulp.series(styles, (cb) =>
+      gulp.src('build/css').pipe(server.stream()).on('end', cb)
+    )
+  );
+  gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload));
+  gulp.watch('package.json', gulp.series(copyDependencies, readyReload));
 
-  return cb()
-}
+  return cb();
+};
